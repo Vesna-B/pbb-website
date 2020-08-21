1f17269b1e245328465d13af3cb9b51ce74b8e86
Vesna-B <babicvesna94@gmail.com>
2020-08-21 16:17:45 +0200

    Responsive header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,21 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { EarthingGraphComponent } from './software/earthing-graph/earthing-graph.component';
 import { HomeComponent } from './info-pages/home/home.component';
-import { from } from 'rxjs';
+//import { from } from 'rxjs';
 import { ReviewEarthPotentialRiseComponent } from './software/review-earth-potential-rise/review-earth-potential-rise.component';
 
 @NgModule({
@@ -40,13 +44,17 @@ import { ReviewEarthPotentialRiseComponent } from './software/review-earth-poten
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule, 
+    FlexLayoutModule,
     MatButtonModule,
     MatToolbarModule,
+    MatSidenavModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatMenuModule
+    MatMenuModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
